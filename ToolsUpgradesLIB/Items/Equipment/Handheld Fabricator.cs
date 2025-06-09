7822e46c4c5687caf0241b4b9082d69eed78f5b4
Law Abiding Troller <colton.trash5.5@proton.me>
2025-06-09 08:30:29 -0500

    New data to update source easier

---
+++ ToolsUpgradesLIB/Items/Equipment/Handheld Fabricator.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace UpgradesLIB.Items.Equipment;
+
+public class Handheldprefab
+{
+    public static CustomPrefab Handheldfab;
+    public static PrefabInfo HandheldfabInfo;
+    public static FabricatorGadget HandheldfabGadget;
+    public static ITreeActionReceiver FabrInstance;
+    public static Vector3 PostScaleValue;
+    public static void Register()
+    {
+        HandheldfabInfo = PrefabInfo.WithTechType("HandHeldToolsFabricator", "Hand Held Upgrades Fabricator",
+                "The hand held fabricator for Upgrades that are specific to tools and equipment.")
+            .WithIcon(SpriteManager.Get(TechType.Fabricator));
+        Handheldfab = new CustomPrefab(HandheldfabInfo);
+        HandheldfabGadget = Handheldfab.CreateFabricator(out HandheldfabTreeType)
+            .AddTabNode("Equipment", "Equipment", SpriteManager.Get(TechType.Fabricator))
+            .AddTabNode("Tools", "Tools", SpriteManager.Get(TechType.Fabricator))
+            .AddCraftNode(HandheldfabInfo.TechType, "Tools")
+            .AddCraftNode(HandheldfabInfo.TechType, "Equipment");
+            
+        var clone = new FabricatorTemplate(HandheldfabInfo, HandheldfabTreeType)
+        {
+            FabricatorModel = FabricatorTemplate.Model.Fabricator,
+            ModifyPrefab = prefab =>
+            { 
+                GameObject model = prefab.gameObject; 
+                model.transform.localScale = Vector3.one / 2f;
+                PostScaleValue = model.transform.localScale;
+                prefab.AddComponent<Pickupable>();
+                prefab.AddComponent<HandHeldFabricator>();
+                List<TechType> compatbats = new List<TechType>()
+                {
+                    TechType.Battery,
+                    TechType.PrecursorIonBattery
+                };
+                PrefabUtils.AddEnergyMixin<BatterySource>(
+                    prefab,
+                    "'I don't really get why it exists, it just decreases the chance of a collision from like 9.399613e-55% to like 8.835272e-111%, both are very small numbers' - Lee23", 
+                    TechType.Battery, 
+                    compatbats);
+            }
+        };
+        Handheldfab.SetGameObject(clone);
+        Handheldfab.SetRecipe(new Nautilus.Crafting.RecipeData()
+            {
+                craftAmount = 1,
+                Ingredients = new List<CraftData.Ingredient>()
+                {
+                    new(TechType.Titanium, 2),
+                    new(TechType.Battery),
+                    new(TechType.Copper),
+                    new(TechType.JeweledDiskPiece, 2)
+                    
+                }
+            })
+            .WithFabricatorType(CraftTree.Type.Fabricator)
+            .WithStepsToFabricatorTab("Personal", "Tools")
+            .WithCraftingTime(3f);
+        Handheldfab.SetUnlock(TechType.Fabricator);
+        Handheldfab.SetEquipment(EquipmentType.Hand);
+        Handheldfab.SetPdaGroupCategory(Plugin.equipmentupgrademodules, Plugin.upgradelib);
+        
+        Handheldfab.Register();
+        Plugin.Logger.LogInfo("Handheld fabricator loaded!");
+        return;
+    }
+
+    public static CraftTree.Type HandheldfabTreeType;
+    }
+
+public class HandHeldFabricator : PlayerTool
+{
+    public override bool OnRightHandDown()
+    {
+        if (Inventory.main.GetHeldTool() is HandHeldFabricator && Inventory.main is not null)
+        {
+            Plugin.HeldTool = Inventory.main.GetHeldTool().GetComponent<Fabricator>();
+            uGUI.main.craftingMenu.Open(Handheldprefab.HandheldfabGadget.CraftTreeType, Plugin.HeldTool);
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ ToolsUpgradesLIB/obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ToolsUpgradesLIB
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.lawabidingmodder.upgradeslib";
+        public const string PLUGIN_NAME = "UpgradesLIB";
+        public const string PLUGIN_VERSION = "0.9.0";
+    }
+}
