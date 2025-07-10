b50723ecaa4f135c32a9543fcbcee132f4607b1a
Law Abiding Coder <colton.trash5.5@proton.me>
2025-07-09 20:06:36 -0500

    Created basic project

---
+++ ToolsUpgrades/obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ToolsUpgrades
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.lawabidingcoder.toolsupgrades";
+        public const string PLUGIN_NAME = "ToolsUpgrades";
+        public const string PLUGIN_VERSION = "0.0.1";
+    }
+}
--- ToolsUpgradesLIB/obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ToolsUpgradesLIB
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.lawabidingmodder.upgradeslib";
-        public const string PLUGIN_NAME = "UpgradesLIB";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
